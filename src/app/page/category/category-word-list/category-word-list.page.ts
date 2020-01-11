fcb4aac0761037a38368e296f69bf588cb3fb1f9
taemin <forest0121@gmail.com>
2020-01-11 14:20:11 +0900

    word view ui

--- src/app/page/category/category-word-list/category-word-list.page.ts
+++ src/app/page/category/category-word-list/category-word-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router,  NavigationExtras } from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-category-word-list',
@@ -11,21 +12,40 @@ export class CategoryWordListPage implements OnInit {
   category_code: string;
  
   word_list : Object[] = [
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111",
-    "111"
+    {
+      "id": "1",
+			"category_code": "BASIC",
+			"category_name": "기초회화",
+			"korean": "나",
+			"chinese": "我",
+			"pronun_ch": "Wǒ",
+			"pronun_kr": "워"
+    },
+    {
+      "id": "2",
+			"category_code": "BASIC",
+			"category_name": "기초회화",
+			"korean": "너",
+			"chinese": "你",
+			"pronun_ch": "Nǐ",
+			"pronun_kr": "니"
+    },
+    {
+      "id": "3",
+			"category_code": "BASIC",
+			"category_name": "기초회화",
+			"korean": "우리",
+			"chinese": "我们",
+			"pronun_ch": "Wǒmen",
+			"pronun_kr": "워먼"
+    }
   ]
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private _location: Location
+  ) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.category_code = this.router.getCurrentNavigation().extras.state.category_code;
@@ -36,4 +56,18 @@ export class CategoryWordListPage implements OnInit {
   ngOnInit() {
   }
 
+  backClicked() {
+    this._location.back();
+  }
+
+  goWordView(word_id){
+    alert(word_id)
+    let navigationExtras: NavigationExtras = {
+      state: {
+        category_code: this.category_code,
+        word_id: word_id
+      }
+    };
+    this.router.navigate(['tabs/category/word-view'], navigationExtras);
+  }
 }
--- src/app/page/category/category-word-view/category-word-view.page.ts
+++ src/app/page/category/category-word-view/category-word-view.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-category-word-view',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryWordViewPage implements OnInit {
 
-  constructor() { }
+  word_id: string;
 
+  word_view : Object = {
+    "id": "1",
+    "category_code": "BASIC",
+    "category_name": "기초회화",
+    "korean": "나",
+    "chinese": "我",
+    "pronun_ch": "Wǒ",
+    "pronun_kr": "워"
+  }
+  
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private _location: Location
+  ) {
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.word_id = this.router.getCurrentNavigation().extras.state.word_id;
+      }
+    });
+  }
   ngOnInit() {
+  }
+
+
+  
+  backClicked() {
+    this._location.back();
   }
 
 }
